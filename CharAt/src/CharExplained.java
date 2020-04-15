ab31fce40e62b2d4fa0beb70bffc96c68b3eb1c6
Taoheed King <tking92@montgomerycollege.edu>
2020-04-14 20:18:35 -0400

    Add files via upload

---
+++ CharAt/src/CharExplained.java
@@ -0,0 +1,18 @@
+public class CharExplained {
+    public static void main(String[] args){
+        String countryName = "Japan";
+        double population = 124.8;
+        String capitalCity = "Tokyo";
+        String currency = "One U.S Dollar equals 107.14 Japanese Yen!!";
+
+        System.out.println("Hey Mom and Dad!");
+        System.out.println("Here are some interesting details about" +
+                "the first country I'm visiting");
+        System.out.println("Name: " + countryName);
+        System.out.println("Population Size: " + population);
+        System.out.println("Capital: " + capitalCity);
+        System.out.println("Currency: " + currency);
+        System.out.println("I'm so excited to visit the Lunar year festival! Talk to you later.");
+
+    }
+}
